9a0440063a8e761010e1e8b2827ceb240c909e4f
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2026-07-17 10:06:34 +0100

    Explain why _last_bytes_received only updated every 100 reads

--- src/paho/mqtt/client.py
+++ src/paho/mqtt/client.py
@@ -3162,6 +3162,9 @@ class Client:
                 self._in_packet['packet'] += data
             count -= 1
             if count == 0:
+                # self._last_bytes_received could be updated on every iteration
+                # (also when count > 0).  Not doing so avoids any
+                # performance overhead from acquiring the lock every iteration.
                 with self._msgtime_mutex:
                     self._last_bytes_received = time_func()
                 return MQTTErrorCode.MQTT_ERR_AGAIN
